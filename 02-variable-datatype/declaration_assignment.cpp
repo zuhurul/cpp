59446e1b9fe8c67d068a8e2e30673fbb0631c7fc
zuhurul <33118938+zuhurul@users.noreply.github.com>
2019-10-20 21:25:04 +0700

    Create declaration_assignment.cpp

---
+++ 02-variable-datatype/declaration_assignment.cpp
@@ -0,0 +1,16 @@
+// Sum: Declare and assignment variable
+// Out: "Length is 10"
+
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+  clrscr();
+  int length; // declaration
+  length = 10; // assignment
+  
+  cout << "Length is " << length << "\n";
+  
+  getch();
+}
