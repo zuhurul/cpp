468d3d62b119e592c7e79ac0b0f750b1fefe6cda
Ahmad Mufid <mufidtk@gmail.com>
2019-10-20 21:12:51 +0700

    Create hello.cpp

---
+++ 01-basic/hello.cpp
@@ -0,0 +1,13 @@
+// Sum: C++ hello world
+// Out: "Hello, world!"
+
+#include <iostream.h>
+#include <conio.h>
+
+int main()
+{
+  clrscr();
+  cout << "Hello, world!\n";
+  getch();
+  return 0;
+}
